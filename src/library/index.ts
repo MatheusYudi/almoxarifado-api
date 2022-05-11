4f05ba660166fecd609f0350e0d06c4ce7c005e6
MatheusYudi <matheusyudi2909@gmail.com>
2022-05-11 01:22:09 -0300

    fix: Adding library exports

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,4 +1,1 @@
-export { BaseController } from "./BaseController";
-export { BaseValidator } from "./BaseValidator";
 export { Database } from "./database/database";
-export { Logger } from "./Logger";
