d398314b2b0fd1c463adbddd7e00251e8c33e762
MatheusYudi <matheusyudi2909@gmail.com>
2022-05-10 23:30:44 -0300

    feat: Adding EnvUtils

---
+++ src/common/utils/EnvUtils.ts
@@ -0,0 +1,28 @@
+/**
+ * EnvUtils
+ *
+ * Classe de utils para tratamento de ambiente
+ */
+export default class EnvUtils {
+    /**
+     * isProduction
+     *
+     * Retorna `true` se o ambiente for de produção
+     *
+     * @returns É produção
+     */
+    public static isProduction(): boolean {
+        return process.env.NODE_ENV === "production";
+    }
+
+    /**
+     * isDevelopment
+     *
+     * Retorna `true` se o ambiente for de desenvolvimento
+     *
+     * @returns É desenvolvimento
+     */
+    public static isDevelopment(): boolean {
+        return process.env.NODE_ENV === "development";
+    }
+}
--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,2 +1,3 @@
-export * from "./ObjectUtils";
-export * from "./StringUtils";
+export * from "@common/utils/EnvUtils";
+export * from "@common/utils/ObjectUtils";
+export * from "@common/utils/StringUtils";
