ca9df4e8bd72a2428302030fcf8cb72ca86a3640
MatheusYudi <matheusyudi2909@gmail.com>
2022-05-11 01:04:22 -0300

    fix: Fixing library exports

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,3 +1,4 @@
 export { BaseController } from "./BaseController";
 export { BaseValidator } from "./BaseValidator";
+export { Database } from "./database/database";
 export { Logger } from "./Logger";
