018d253ad9041e780ae8ba30c4ecc3815b5f7fe3
MatheusYudi <matheusyudi2909@gmail.com>
2022-05-28 18:41:52 -0300

    feat: adding token/crypto utils

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,5 +1,7 @@
 export * from "./CNPJUtils";
 export * from "./CPFUtils";
+export * from "./CryptoUtils";
 export * from "./EnvUtils";
 export * from "./ObjectUtils";
 export * from "./StringUtils";
+export * from "./TokenUtils";
